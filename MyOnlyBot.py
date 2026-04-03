cc77210b677d4e344f067e2ccb1a03b3ea923223
ivan8yabko-stack <ivan8yabko@icloud.com>
2026-04-03 19:12:12 +0200

    Update MyOnlyBot.py

--- MyOnlyBot.py
+++ MyOnlyBot.py
@@ -23,12 +23,6 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 from aiogram.fsm.context import FSMContext
 
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-
 def get_driver():
     options = Options()
     options.add_argument("--headless=new")
